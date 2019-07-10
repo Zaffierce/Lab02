184f756f8ae57f39730ec639d0f84215fba44fe4
Corey Marchand <corey.marchand@me.com>
2019-07-10 00:55:54 -0700

    Added javascript questions and about me page

---
+++ Js/app.js
@@ -0,0 +1,65 @@
+// var doesCoreyLikePizza = prompt('Does Corey like pizza?'); 
+
+
+// if (doesCoreyLikePizza.toLowerCase() === 'yes') {
+//   alert('Absolutely he likes pizza');
+// } else {
+//   alert('You have entered the complete wrong answer, TRY AGAIN');
+// }
+
+// while(condition is !== yes){
+//  prompt(Wrong answer, try again)
+
+var doesCoreyLikePizza = prompt('Does Corey like Pizza?');
+if (doesCoreyLikePizza.toLowerCase() === 'yes' && 'y') {
+  alert('Absolutely Correct!');
+} else if (doesCoreyLikePizza.toLowerCase() != 'no' && 'n') {
+  alert('WRONG!');
+} else {
+  alert('WRONG!');
+}
+
+var doesCoreyLikeTheOffice = prompt('Does Corey like the Office?');
+if (doesCoreyLikeTheOffice.toLowerCase() === 'yes' && 'y') {
+  alert('Abosultely Right!');
+} else if (doesCoreyLikeTheOffice.toLowerCase() != 'no'&&'n') {
+  alert('Wrong anwser!');
+} else {
+  alert('Wrong answer!');
+}
+
+var doesCoreyLikeWine = prompt('Does Corey like wine?');
+if (doesCoreyLikeWine.toLowerCase() === 'yes' && 'y') {
+  alert('Abosultely wrong!');
+} else if (doesCoreyLikeWine.toLowerCase() != 'no' && 'n') {
+  alert('Correct!');
+} else {
+  alert('Correct!');
+}
+
+var doesCoreyLikeSalad = prompt('Does Corey like Salads?');
+if (doesCoreyLikeSalad.toLowerCase() === 'yes' && 'y') {
+  alert('Abosultely Right!');
+} else if (doesCoreyLikeSalad.toLowerCase() != 'no'&&'n') {
+  alert('Wrong anwser!');
+} else {
+  alert('Wrong answer!');
+}
+
+var doesCoreyLikeNeckCramps = prompt('Does Corey like neck cramps?');
+if (doesCoreyLikeNeckCramps.toLowerCase() === 'yes' && 'y') {
+  alert('Abosultely wrong!');
+} else if (doesCoreyLikeNeckCramps.toLowerCase() != 'no'&&'n') {
+  alert('Right anwser!');
+} else {
+  alert('Right answer!');
+}
+
+// var likesStrangerThings = prompt('Do you like Stranger Things?');
+// likesStrangerThings = likesStrangerThings.toLowerCase();
+
+// if(likesStrangerThings === 'yes'){
+//   alert('I like it too!');
+// } else {
+//   alert('That sucks');
+// }
