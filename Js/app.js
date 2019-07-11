7bb86039a8e5dd50c9b03b3c89c22f07c65c8ef8
Corey Marchand <corey.marchand@me.com>
2019-07-11 00:10:18 -0700

    Added more js

--- Js/app.js
+++ Js/app.js
@@ -72,15 +72,15 @@ var guessCounter = 0;
 
 while(guessCounter < 3) {
 
-  var guessCoreysFavoriteDog = prompt('What is Corey\'s favorite Dog?');
+  var guessCoreysFavoriteDog = prompt('What is Corey\'s favorite type of Dog?');
 
-  if(guessCoreysFavoriteDog === coreysFavoriteDog) {
+  if(guessCoreysFavoriteDog.toLowerCase === coreysFavoriteDog.toLowerCase) {
     alert('Great Job!!!')
     break;
   } else {
     alert('Your the worst')
   }
-  console.log('My favorite Dog is a ' + coreysFavoriteDog)
+  console.log('My favorite Dog is a ' + coreysFavoriteDog);
   guessCounter++;
 }
 
@@ -93,13 +93,13 @@ while(guessCounter < 3) {
 
   var guessCoreysFavoriteFood = prompt('What is Corey\'s favorite food?');
 
-  if(guessCoreysFavoriteFood === coreysFavoriteFood) {
+  if(guessCoreysFavoriteFood.toLowerCase === coreysFavoriteFood.toLowerCase) {
     alert('Great Job!!!')
     break;
   } else {
     alert('Pathetic')
   }
-  console.log('My favorite food is ' + coreysFavoriteFood)
+  console.log('My favorite food is ' + coreysFavoriteFood);
   guessCounter++;
 }
 
@@ -110,29 +110,67 @@ guessCounter = 0;
 while(guessCounter < 3) {
   var guessCoreysFavoriteSport = prompt('What is Corey\'s favorite sport?');
 
-  if(guessCoreysFavoriteSport === coreysFavoriteSport) {
-    alert('Great Job, nicely done1!')
+  if(guessCoreysFavoriteSport.toLowerCase === coreysFavoriteSport.toLowerCase) {
+    alert('Great Job, nicely done!')
     break;
   } else {
-    alert('You shouldn/t have been born')
+    alert('You shouldn\'t have been born')
   }
-  console.log('My favorite sport is ' + coreysFavoriteSport)
+  console.log('My favorite sport is ' + coreysFavoriteSport);
   guessCounter++;
 }
 
+var coreysFavoriteTvShowArray = ['It\s alawys sunny in philidalphia', 'the Office', 'Vikings', 'Game of Thrones'];
+var coreysFavoriteTvShow = 'It\'s always sunny in Philidalphia';
+guessCounter = 0;
 
+while(guessCounter < 3) {
+  var guessCoreysFavoriteTvShow = prompt('What is Corey\'s favorite TV Show?');
 
+  if(guessCoreysFavoriteTvShow.toLowerCase === coreysFavoriteTvShow.toLowerCase) {
+    alert('Correct, Corey only watches quality TV')
+    break;
+  } else {
+    alert('I\'m disappointed')
+  }
+  console.log('My favorite tv show is ' + coreysFavoriteTvShow)
+  guessCounter++;
+}
 
+var coreysMostHatedChoreArray = ['Taking out the trash', 'cleaning the shower', 'cleaning the toilet', 'washing dishes'];
+var coreysMostHatedChore = 'cleaning the shower';
+guessCounter = 0;
 
+while(guessCounter < 3) {
+  var guessCoreysMostHatedChore = prompt('What is Corey\'s least favorite chore?');
 
+  if(guessCoreysMostHatedChore.toLowerCase === coreysMostHatedChore.toLowerCase) {
+    alert('Correct, How do showers even get dirty? It Only makes sense that it would be perfectly clean all the time.')
+    break;
+  } else {
+    alert('You should try harder, not impressed by you')
+  }
+  console.log('I hate cleaning the shower, like I really despise ' + coreysMostHatedChore)
+  guessCounter++;
+}
 
-// for (var i = 0; i < 4; i++) {
+var i = 0;
+var x = true;
 
-//   if (coreysFavoriteDog === guessCoreysFavoriteDog) {
-//     console.log('My favorite Dog is ' + coreysFavoriteDog);
-//   alert('Congrats, you are correct!');
-// } else {
-//   alert('Wrong answer, try again!');
-// }
+while(i < 4 && x === true){   
+     var favNum = prompt('What is my favorite number?');  
 
-// }
+       if (Number(favNum) === 1){
+       alert('Correct!!');
+       x = false;
+       break;
+   }
+
+   else if (favNum > 1) {
+       alert('Good try! The number is lower than that! You have used ' + (i+1))
+       } else if (favNum < 1) {
+       alert('Good try! The number is higher than that! You have used ' + (i+1) + ' of your 4 tries!')
+       }
+       console.log('my favorite number is 1')
+       i++;
+}
