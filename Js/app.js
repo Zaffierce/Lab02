21ee6bbed1305f6e7a27ffcc5252739f3128e236
Jon Veach <veach09@gmail.com>
2019-07-12 08:53:11 -0700

    Created/fixed functions for questions

--- Js/app.js
+++ Js/app.js
@@ -1,176 +1,324 @@
-// var doesCoreyLikePizza = prompt('Does Corey like pizza?'); 
+'use strict';
+
+// // var doesCoreyLikePizza = prompt('Does Corey like pizza?'); 
 
 
-// if (doesCoreyLikePizza.toLowerCase() === 'yes') {
-//   alert('Absolutely he likes pizza');
-// } else {
-//   alert('You have entered the complete wrong answer, TRY AGAIN');
-// }
+// // if (doesCoreyLikePizza.toLowerCase() === 'yes') {
+// //   alert('Absolutely he likes pizza');
+// // } else {
+// //   alert('You have entered the complete wrong answer, TRY AGAIN');
+// // }
 
-// while(condition is !== yes){
-//  prompt(Wrong answer, try again)
+// // while(condition is !== yes){
+// //  prompt(Wrong answer, try again)
 
-// var doesCoreyLikePizza = prompt('Does Corey like Pizza?');
-// if (doesCoreyLikePizza.toLowerCase() === 'yes' || doesCoreyLikePizza.toLowerCase() === 'y') {
-//   alert('Absolutely Correct!');
-// } else if (doesCoreyLikePizza.toLowerCase() != 'no' || doesCoreyLikePizza.toLowerCase() != 'n') {
-//   alert('WRONG!');
-// } else {
-//   alert('WRONG!');
-// }
+// // var doesCoreyLikePizza = prompt('Does Corey like Pizza?');
+// // if (doesCoreyLikePizza.toLowerCase() === 'yes' || doesCoreyLikePizza.toLowerCase() === 'y') {
+// //   alert('Absolutely Correct!');
+// // } else if (doesCoreyLikePizza.toLowerCase() != 'no' || doesCoreyLikePizza.toLowerCase() != 'n') {
+// //   alert('WRONG!');
+// // } else {
+// //   alert('WRONG!');
+// // }
 
-// var doesCoreyLikeTheOffice = prompt('Does Corey like the Office?');
-// if (doesCoreyLikeTheOffice.toLowerCase() === 'yes' || doesCoreyLikeTheOffice.toLocaleLowerCase() === 'y') {
-//   alert('Abosultely Right!');
-// } else if (doesCoreyLikeTheOffice.toLowerCase() != 'no' || doesCoreyLikeTheOffice.toLowerCase() != 'n') {
-//   alert('Wrong anwser!');
-// } else {
-//   alert('Wrong answer!');
-// }
+// // var doesCoreyLikeTheOffice = prompt('Does Corey like the Office?');
+// // if (doesCoreyLikeTheOffice.toLowerCase() === 'yes' || doesCoreyLikeTheOffice.toLocaleLowerCase() === 'y') {
+// //   alert('Abosultely Right!');
+// // } else if (doesCoreyLikeTheOffice.toLowerCase() != 'no' || doesCoreyLikeTheOffice.toLowerCase() != 'n') {
+// //   alert('Wrong anwser!');
+// // } else {
+// //   alert('Wrong answer!');
+// // }
 
-// var doesCoreyLikeWine = prompt('Does Corey like wine?');
-// if (doesCoreyLikeWine.toLowerCase() === 'yes' || doesCoreyLikeWine.toLowerCase() === 'y') {
-//   alert('Abosultely wrong!');
-// } else if (doesCoreyLikeWine.toLowerCase() != 'no' || doesCoreyLikeWine.toLowerCase() != 'n') {
-//   alert('Correct!');
-// } else {
-//   alert('Correct!');
-// }
+// // var doesCoreyLikeWine = prompt('Does Corey like wine?');
+// // if (doesCoreyLikeWine.toLowerCase() === 'yes' || doesCoreyLikeWine.toLowerCase() === 'y') {
+// //   alert('Abosultely wrong!');
+// // } else if (doesCoreyLikeWine.toLowerCase() != 'no' || doesCoreyLikeWine.toLowerCase() != 'n') {
+// //   alert('Correct!');
+// // } else {
+// //   alert('Correct!');
+// // }
 
-// var doesCoreyLikeSalad = prompt('Does Corey like Salads?');
-// if (doesCoreyLikeSalad.toLowerCase() === 'yes' || doesCoreyLikeSalad.toLowerCase() === 'y') {
-//   alert('Abosultely Right!');
-// } else if (doesCoreyLikeSalad.toLowerCase() != 'no' || doesCoreyLikeSalad.toLowerCase != 'n') {
-//   alert('Wrong anwser!');
-// } else {
-//   alert('Wrong answer!');
-// }
+// // var doesCoreyLikeSalad = prompt('Does Corey like Salads?');
+// // if (doesCoreyLikeSalad.toLowerCase() === 'yes' || doesCoreyLikeSalad.toLowerCase() === 'y') {
+// //   alert('Abosultely Right!');
+// // } else if (doesCoreyLikeSalad.toLowerCase() != 'no' || doesCoreyLikeSalad.toLowerCase != 'n') {
+// //   alert('Wrong anwser!');
+// // } else {
+// //   alert('Wrong answer!');
+// // }
 
-// var doesCoreyLikeNeckCramps = prompt('Does Corey like neck cramps?');
-// if (doesCoreyLikeNeckCramps.toLowerCase() === 'yes' || doesCoreyLikeNeckCramps.toLowerCase() === 'y') {
-//   alert('Abosultely wrong!');
-// } else if (doesCoreyLikeNeckCramps.toLowerCase() != 'no' || doesCoreyLikeNeckCramps.toLowerCase() != 'n') {
-//   alert('Right anwser!');
-// } else {
-//   alert('Right answer!');
-// }
+// // var doesCoreyLikeNeckCramps = prompt('Does Corey like neck cramps?');
+// // if (doesCoreyLikeNeckCramps.toLowerCase() === 'yes' || doesCoreyLikeNeckCramps.toLowerCase() === 'y') {
+// //   alert('Abosultely wrong!');
+// // } else if (doesCoreyLikeNeckCramps.toLowerCase() != 'no' || doesCoreyLikeNeckCramps.toLowerCase() != 'n') {
+// //   alert('Right anwser!');
+// // } else {
+// //   alert('Right answer!');
+// // }
 
-// var likesStrangerThings = prompt('Do you like Stranger Things?');
-// likesStrangerThings = likesStrangerThings.toLowerCase();
+// // var likesStrangerThings = prompt('Do you like Stranger Things?');
+// // likesStrangerThings = likesStrangerThings.toLowerCase();
 
-// if(likesStrangerThings === 'yes'){
-//   alert('I like it too!');
-// } else {
-//   alert('That sucks');
+// // if(likesStrangerThings === 'yes'){
+// //   alert('I like it too!');
+// // } else {
+// //   alert('That sucks');
 
-// }
+// // }
 
 
-var coreysFavoriteDogArray = ['Golden Retriever','bulldog','german sheppard','labs'];
-var coreysFavoriteDog = 'Golden Retriever';
-var guessCounter = 0; 
+// //Question of Coreys favorite dog type 
+// // function askAQuestion(Question) {
+// //    answer = prompt(Question);
+// // }
+  
+// //   function whatIsYourFavoriteDog() {
+// //     var coreysFavoriteDog = 'Golden Retriever';
+// //     var guessCounter = 0; 
+    
+// //     while(guessCounter < 3) {
+// //       askAQuestion('What Type of Dog is your favorite');
+// //       guessCoreysFavoriteDog = answer;
+// //       if(guessCoreysFavoriteDog.toLowerCase() === coreysFavoriteDog.toLowerCase()){
+// //         alert('Great Job!!!')
+// //         break;
+// //       } else {
+// //         alert('Your the worst')
+// //       }
+// //       console.log('My favorite Dog is a ' + coreysFavoriteDog);
+// //       guessCounter++;
+// //     }
+// //   }
+// //   whatIsYourFavoriteDog();
+  
 
-while(guessCounter < 3) {
+// // // // Question 2: favorite food 
 
-  var guessCoreysFavoriteDog = prompt('What is Corey\'s favorite type of Dog?');
+// // function whatIsYourFavoriteFood() {
+// //   var coreysFavoriteFood = 'Pizza';
+// //   guessCounter = 0; 
 
-  if(guessCoreysFavoriteDog.toLowerCase === coreysFavoriteDog.toLowerCase) {
-    alert('Great Job!!!')
-    break;
-  } else {
-    alert('Your the worst')
+// //   while(guessCounter < 3) {
+// //     askAQuestion('What is Corey\'s favorite food?');
+// //     guessCoreysFavoriteFood = answer;
+
+// //     if(guessCoreysFavoriteFood.toLowerCase() === coreysFavoriteFood.toLowerCase()) {
+// //       alert('Great Job!!!')
+// //       break;
+// //     } else {
+// //       alert('Pathetic')
+// //     }
+// //     console.log('My favorite food is ' + coreysFavoriteFood);
+// //     guessCounter++;
+// //   }
+// // }
+// // whatIsYourFavoriteFood();
+
+// // //Question 3: Favorite sports 
+// // function whatIsYourFavoriteSport() {
+// //   var coreysFavoriteSport = 'Lacrosse';
+// //   guessCounter = 0;
+
+// //   while(guessCounter < 3) {
+// //     askAQuestion('What is Corey\s favorite Sport?');
+// //     guessCoreysFavoriteSport = answer;
+
+// //     if(guessCoreysFavoriteSport.toLowerCase() === coreysFavoriteSport.toLowerCase()) {
+// //       alert('Great Job, nicely done!')
+// //       break;
+// //     } else {
+// //       alert('You shouldn\'t have been born')
+// //     }
+// //     console.log('My favorite sport is ' + coreysFavoriteSport);
+// //     guessCounter++;
+// //   }
+// // }
+// // whatIsYourFavoriteSport();
+
+// // //Question 4: Favorite TV Show
+// // function whatIsYourFavoriteTvShow() {
+// //   var coreysFavoriteTvShow = 'It\'s always sunny in Philidalphia';
+// //   guessCounter = 0;
+
+// //   while(guessCounter < 3) {
+// //     askAQuestion('What is Corey\s favorite TV show?');
+// //     guessCoreysFavoriteTvShow = answer;
+
+// //     if(guessCoreysFavoriteTvShow.toLowerCase() === coreysFavoriteTvShow.toLowerCase()) {
+// //       alert('Correct, Corey only watches quality TV')
+// //       break;
+// //     } else {
+// //       alert('I\'m disappointed')
+// //     }
+// //     console.log('My favorite tv show is ' + coreysFavoriteTvShow)
+// //     guessCounter++;
+// //   }
+// // }
+// // whatIsYourFavoriteTvShow(); 
+
+// // // // Question 5: Most hated Chore
+// // function whatIsYourMostHatedChore() {
+// //   var coreysMostHatedChore = 'cleaning the shower';
+// //   guessCounter = 0;
+
+// //   while(guessCounter < 3) {
+// //     askAQuestion('What is Corey\'s least favorite chore?')
+// //     guessCoreysMostHatedChore = answer;
+
+// //     if(guessCoreysMostHatedChore.toLowerCase() === coreysMostHatedChore.toLowerCase()) {
+// //       alert('Correct, How do showers even get dirty? It Only makes sense that it would be perfectly clean all the time.')
+// //       break;
+// //     } else {
+// //       alert('You should try harder, not impressed by you')
+// //     }
+// //     console.log('I hate cleaning the shower, like I really despise ' + coreysMostHatedChore)
+// //     guessCounter++;
+// //   }
+// // }
+// // whatIsYourMostHatedChore();
+
+// // //Queston 6: My favorite Number
+// // var i = 0;
+// // var x = true;
+
+// // while(i < 4 && x === true){   
+// //      var favNum = prompt('What is my favorite number?');  
+
+// //        if (Number(favNum) === 1){
+// //        alert('Correct!!');
+// //        x = false;
+// //        break;
+// //    }
+
+// //    else if (favNum > 1) {
+// //        alert('Good try! The number is lower than that! You have used ' + (i+1))
+// //        } else if (favNum < 1) {
+// //        alert('Good try! The number is higher than that! You have used ' + (i+1) + ' of your 4 tries!')
+// //        }
+// //        console.log('my favorite number is 1')
+// //        i++;
+// // }
+
+
+
+// Identifying global variables 
+
+var numberOfCorrect = 0;
+var numberOfQuestion = 1;
+
+// creating a loop
+
+function loop(guessAttemptsCounter, guessNumber, coreysQuestion, coreysAnswer, questionNumber) {
+  var endLoop = false; 
+  
+  while (guessAttemptsCounter < guessNumber && endLoop === false) {
+    var userAnswer = prompt(`Let\'s see how well you can do on a quiz all about me!\n\n Question ${numberOfQuestion}: ${coreysQuestion}`);
+    for (var i = 0; i < coreysAnswer.length; i++) {
+      if (userAnswer.toLowerCase() === coreysAnswer[i]) {
+        alert('You are Right! Congratulations, keep going.');
+        numberOfCorrect++;
+        coreysQuestion;
+        questionNumber;
+        numberOfQuestion++;
+        console.log(`Question loop : ${numberOfQuestion}`);
+        endLoop = true;
+        break;
+      }
+      guessAttemptsCounter++;
+    }
   }
-  console.log('My favorite Dog is a ' + coreysFavoriteDog);
-  guessCounter++;
-}
-
-
-var coreysFavoriteFoodArray = ['Pizza', 'Spaghetti', 'Pasta', 'Cheese'];
-var coreysFavoriteFood = 'Pizza';
-guessCounter = 0; 
-
-while(guessCounter < 3) {
-
-  var guessCoreysFavoriteFood = prompt('What is Corey\'s favorite food?');
-
-  if(guessCoreysFavoriteFood.toLowerCase === coreysFavoriteFood.toLowerCase) {
-    alert('Great Job!!!')
-    break;
-  } else {
-    alert('Pathetic')
+  if (guessAttemptsCounter > guessNumber) {
+    endLoop = true;
+    numberOfQuestion++;
+    alert ('You are wrong! Keep going!');
   }
-  console.log('My favorite food is ' + coreysFavoriteFood);
-  guessCounter++;
 }
 
-var coreysFavoriteSportArray = ['Lacrosse', 'baseball', 'football','basketball'];
-var coreysFavoriteSport = 'Lacrosse';
-guessCounter = 0;
 
-while(guessCounter < 3) {
-  var guessCoreysFavoriteSport = prompt('What is Corey\'s favorite sport?');
-
-  if(guessCoreysFavoriteSport.toLowerCase === coreysFavoriteSport.toLowerCase) {
-    alert('Great Job, nicely done!')
-    break;
-  } else {
-    alert('You shouldn\'t have been born')
-  }
-  console.log('My favorite sport is ' + coreysFavoriteSport);
-  guessCounter++;
+// Function of my favorite dog type 
+function coreysFavoriteDog() {
+  var questionNumber = 'First';
+  var coreysQuestion = 'What is Corey\'s favorite type of dog?';
+  var coreysAnswer = ['golden retriever'];
+  var guessNumber = 3;
+  var guessAttemptsCounter = 0;
+  loop(guessAttemptsCounter, guessNumber, coreysQuestion, coreysAnswer, questionNumber);
 }
+coreysFavoriteDog();
 
-var coreysFavoriteTvShowArray = ['It\s alawys sunny in philidalphia', 'the Office', 'Vikings', 'Game of Thrones'];
-var coreysFavoriteTvShow = 'It\'s always sunny in Philidalphia';
-guessCounter = 0;
 
-while(guessCounter < 3) {
-  var guessCoreysFavoriteTvShow = prompt('What is Corey\'s favorite TV Show?');
 
-  if(guessCoreysFavoriteTvShow.toLowerCase === coreysFavoriteTvShow.toLowerCase) {
-    alert('Correct, Corey only watches quality TV')
-    break;
-  } else {
-    alert('I\'m disappointed')
-  }
-  console.log('My favorite tv show is ' + coreysFavoriteTvShow)
-  guessCounter++;
+// Function of Favorite food
+function coreysFavoriteFood() {
+  var questionNumber = 'Second';
+  var coreysQuestion = 'What is Corey\'s favorite food?';
+  var coreysAnswer = 'Pizza';
+  var guessNumber = 3;
+  var guessAttemptsCounter = 0;
+  loop(guessAttemptsCounter, guessNumber, coreysQuestion, coreysAnswer, questionNumber);
 }
+coreysFavoriteFood();
 
-var coreysMostHatedChoreArray = ['Taking out the trash', 'cleaning the shower', 'cleaning the toilet', 'washing dishes'];
-var coreysMostHatedChore = 'cleaning the shower';
-guessCounter = 0;
 
-while(guessCounter < 3) {
-  var guessCoreysMostHatedChore = prompt('What is Corey\'s least favorite chore?');
 
-  if(guessCoreysMostHatedChore.toLowerCase === coreysMostHatedChore.toLowerCase) {
-    alert('Correct, How do showers even get dirty? It Only makes sense that it would be perfectly clean all the time.')
-    break;
-  } else {
-    alert('You should try harder, not impressed by you')
-  }
-  console.log('I hate cleaning the shower, like I really despise ' + coreysMostHatedChore)
-  guessCounter++;
+// Function of Favorite Sport 
+function coreysFavoriteSport() {
+  var questionNumber = 'Third';
+  var coreysQuestion = 'What is Corey\'s favorite Sport?';
+  var coreysAnswer = 'Lacrosse';
+  var guessNumber = 3;
+  var guessAttemptsCounter = 0;
+  loop(guessAttemptsCounter, guessNumber, coreysQuestion, coreysAnswer, questionNumber);
 }
+coreysFavoriteSport();
 
-var i = 0;
-var x = true;
 
-while(i < 4 && x === true){   
-     var favNum = prompt('What is my favorite number?');  
 
-       if (Number(favNum) === 1){
-       alert('Correct!!');
-       x = false;
-       break;
-   }
+// Function of Favorite tv show
 
-   else if (favNum > 1) {
-       alert('Good try! The number is lower than that! You have used ' + (i+1))
-       } else if (favNum < 1) {
-       alert('Good try! The number is higher than that! You have used ' + (i+1) + ' of your 4 tries!')
-       }
-       console.log('my favorite number is 1')
-       i++;
+function coreysFavoriteTvShow() {
+  var questionNumber = 'Fourth';
+  var coreysQuestion = 'What is Corey\'s favorite TV show?';
+  var coreysAnswer = 'It\'s always Sunny In Philalphia';
+  var guessNumber = 3;
+  var guessAttemptsCounter = 0;
+  loop(guessAttemptsCounter, guessNumber, coreysQuestion, coreysAnswer, questionNumber);
 }
+coreysFavoriteTvShow();
+
+
+
+// Function of Least favorite chore
+
+function coreysLeastFavoriteChore() {
+  var questionNumber = 'Fifth';
+  var coreysQuestion = 'What is Corey\'s least favorite chore?';
+  var coreysAnswer = 'Cleaning the shower';
+  var guessNumber = 3;
+  var guessAttemptsCounter = 0;
+  loop(guessAttemptsCounter, guessNumber, coreysQuestion, coreysAnswer, questionNumber);
+}
+coreysLeastFavoriteChore();
+
+
+
+// Function on what different states Ive lived in
+
+function coreysStatesLivedIn(); {
+  var questionNumber = 'sixth';
+  var coreysQuestion = 'What are the different states that Corey has live in?';
+  var coreysAnswer = ['Washington', 'Virginia', 'South Carolina', 'Texas', 'Alabama', 'Missouri']; 
+  var guessNumber = 3;
+  var guessAttemptsCounter = 0;
+  loop(guessAttemptsCounter, guessNumber, coreysQuestion, coreysAnswer, questionNumber);
+}
+coreysStatesLivedIn();
+
+
+
+// Function giving user prompt on their score 
+function finalScore() {
+  alert(`You scored a ${numOfCorrect} out of ${numOfQuestion-1} you are now done with the test. Congrats?`); 
+}
+finalScore();
